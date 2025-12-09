7093d4c279acf8f12c557af902114f7591027749
unknown <pabloszef2115@gmail.com>
2025-12-09 21:08:58 +0100

    Finish the auth callback mechanism and implement basic redis code

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+"use client"
+
 import {Loader} from "lucide-react";
+import {useQuery} from "@tanstack/react-query";
+import {checkAuthStatus} from "@/actions/auth.acions";
+import {useRouter} from "next/navigation";
+
 
 function Page() {
+    const router = useRouter();
+    const {data} = useQuery({
+        queryKey: ["authCheck"],
+        queryFn: async () => await checkAuthStatus()
+    });
+
+    if(data?.success) router.push("/")
+
     return (
         <div className="mt-20 w-full flex justify-center">
             <div className="flex flex-col items-center gap-2">
