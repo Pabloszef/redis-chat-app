8dbe7ac4041f83b60a32e4575fbe7b515bcce2b0
unknown <pabloszef2115@gmail.com>
2025-12-09 18:48:20 +0100

    Finish Auth Callback Design

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Loader} from "lucide-react";
 
 function Page() {
     return (
-        <div>Auth callback</div>
+        <div className="mt-20 w-full flex justify-center">
+            <div className="flex flex-col items-center gap-2">
+                <Loader className="size-10 animate-spin text-muted-foreground"/>
+                <h3 className="text-xl font-bold">
+                    Redirecting...
+                </h3>
+
+                <p>Please wait</p>
+            </div>
+        </div>
     )
 }
 
